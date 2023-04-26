c8ad1e570cf4ec076a802d56c992640e54cd591b
Дима Случак <pripru916@gmail.com>
2023-04-27 01:30:09 +0300

    Добавьте файлы проекта.

---
+++ Task_20_PolessUP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+
+            for (int i = 3; i <= n; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0) break;
+                    if (j == i - 1) counter++;
+                }
+            }
+            Console.WriteLine("Вывод:" +counter);
+            Console.ReadKey();
+        }
+    }
+}
